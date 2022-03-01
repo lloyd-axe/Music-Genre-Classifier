55d24f2410945f968ab8bee0bb84efc66144cbda
MSI\Lloyd Acha <lloydacha@gmail.com>
2022-03-01 21:31:11 +0800

    Update v1.0

--- ModelUtils.py
+++ ModelUtils.py
@@ -9,8 +9,13 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
 from tensorflow.keras.layers import BatchNormalization, Conv2D, Dense, Dropout, Flatten, MaxPool2D
+'''
+This module is specifically made to create a deep learning model
+with the "GTZAN Dataset - Music Genre Classification" Dataset.
 
-class ModelBuilderModule:  
+Nothing special here, just classic tensorflow methods.
+'''
+class ModelUtils:  
     @staticmethod
     def prepare_data(
         data_path, 
@@ -21,19 +26,26 @@ class ModelBuilderModule:
         n_mfcc = 13, 
         n_fft = 2048, 
         hop_len = 512):
-        
+        '''
+        Like I said above, this method is specifically made to collect audio data
+        from the "GTZAN Dataset - Music Genre Classification" Dataset.
+        Other datasets can be collected too as long as the dataset structure is similar.
+
+        The method will walk into all the genre folders in the dataset. Each genre name will
+        be stored in the 'mapping' key of {data}. Then, the mfcc data for each audio file will
+        be collected and stored into the 'mfcc' key of {data}. The corresponding labels will 
+        also be stored in the 'labels' key of {data}.
+        '''
         data ={'mapping' : [],'mfcc' : [],'labels' : []}
         samp_p_track = sr * duration
         samp_p_seg = int(samp_p_track / n_segments)
         mfcc_p_seg = math.ceil(samp_p_seg / hop_len)
-        
-        #walk in data_path
         for i, (dirpath, dirname, filenames) in enumerate(os.walk(data_path)):
             if dirpath is not data_path:
                 label = dirpath.split('\\')[-1]
-                data['mapping'].append(label)
+                data['mapping'].append(label) #Add genre name
                 print(f'Processing {label} - {len(filenames)}x...')
-                for file in filenames:
+                for file in filenames: #Collect mfcc data from each audio file on genre
                     file_path = os.path.join(dirpath, file)
                     signal, sr = librosa.load(file_path, sr = sr)
                     print(signal.shape)
@@ -48,6 +60,7 @@ class ModelBuilderModule:
                         if len(mfcc) == mfcc_p_seg:
                             data['mfcc'].append(mfcc.tolist())
                             data['labels'].append(i-1)
+        #Saving data into json
         with open(json_path, 'w') as fp:
             json.dump(data, fp, indent=4)
             print(f'Data saved to {json_path}')
@@ -68,16 +81,13 @@ class ModelBuilderModule:
                                                             y, 
                                                             test_size = test_size)
         
-        
         if val_set:
             x_train, x_val, y_train, y_val = train_test_split(x_train,
                                                                 y_train, 
                                                                 test_size = val_size)
             x_val = np.expand_dims(x_val, axis=3)
-
         x_train = np.expand_dims(x_train, axis=3)
         x_test = np.expand_dims(x_test, axis=3)
-
         inputs['x_train'] = x_train
         inputs['x_test'] = x_test
         inputs['x_val'] = x_val
@@ -87,7 +97,7 @@ class ModelBuilderModule:
         return inputs
     
     @staticmethod
-    def createmodel(shape, n_class):
+    def create_model(shape, n_class):
         model = Sequential()
         model.add(Conv2D(32, (3,3),
                         activation='relu',
@@ -119,14 +129,13 @@ class ModelBuilderModule:
         y, 
         epochs, 
         val_data = ([], []), 
-        modelname='model', 
+        model_name='model', 
         plot = False, 
         earlyStop = True):
         if not os.path.isdir('checkpoints'):
             os.mkdir('checkpoints')
             print('checkpoints folder made')
         check_model_path = 'checkpoints/mdl-{epoch:02d}-{val_categorical_accuracy:2f}.hdf5'
-        check_weights_path = "checkpoints/cp-{epoch:04d}.ckpt"
         model_check = ModelCheckpoint(check_model_path, 
                                       monitor='val_categorical_accuracy', 
                                       verbose = 1, 
@@ -159,6 +168,11 @@ class ModelBuilderModule:
                 plt.ylabel('categorical_accuracy')
                 plt.legend()
                 plt.xlim([0,max(history.epoch)])
-        model.save(modelname + '.h5')
+        model.save(model_name + '.h5')
         return model
+
+    @staticmethod
+    def save_model(model, name):
+        path = './{}_bert'.format(name.replace('/', '_'))
+        model.save(path, include_optimizer=False)
     
--- MusicGenreClassifier.py
+++ MusicGenreClassifier.py
@@ -1,8 +1,48 @@
 import math
 import librosa
 import numpy as np
+'''
+Module for properly using the trained model to predict any audio file.
+But for now, it can only predict .wav files.
 
+Plan to implement converter in future versions.
+'''
 class MusicGenreClassifier:
+    @staticmethod
+    def get_sample(
+        file_path, 
+        sr = 22050, 
+        duration = 3, 
+        n_segments = 5, 
+        n_mfcc = 13, 
+        n_fft = 2048, 
+        hop_len = 512):
+        #As mentioned above, the module will can only convert .wav files.
+        if file_path.split('\\')[-1].split('.')[-1] != 'wav':
+            print('Only .wav files are allowed for now.')
+            return None
+        samp_p_track = sr * duration
+        samp_p_seg = int(samp_p_track / n_segments)
+        mfcc_p_seg = math.ceil(samp_p_seg / hop_len)
+        
+        mfcc_data = []
+        '''
+        The audiofile will be divided into parts. Each part will be {duration} long.
+        The mfcc for each part will be stored in a list
+        '''
+        print(f'Loading audio file...')
+        signal, sr = librosa.load(file_path, sr = sr)
+        for part in range((signal.shape[0]//samp_p_track)):
+            sig = signal[samp_p_track*part : (samp_p_track*part) + samp_p_track]
+            for seg in range(n_segments):
+                mfcc = librosa.feature.mfcc(
+                    y = sig[samp_p_seg * seg:(samp_p_seg * seg) + samp_p_seg], 
+                    sr = sr, n_mfcc = n_mfcc, 
+                    n_fft = n_fft, hop_length = hop_len)
+                mfcc = mfcc.T
+                if len(mfcc) == mfcc_p_seg:
+                    mfcc_data.append(mfcc.tolist())
+        return mfcc_data
     @staticmethod
     def predict_genre(model, data):
         data = np.expand_dims(np.array(data), axis=3)
@@ -18,43 +58,14 @@ class MusicGenreClassifier:
             'reggae',
             'rock'
             ]
+        '''
+        The model will predict the genre of each mfcc in the {data}.
+        Then, the most reoccuring genre will be considered as the main genre
+        '''
         pred_idx = []  
         pred = model.predict(data)
         for p in pred:
             pred_idx.append(np.argmax(p))
         return label_map[max(pred_idx, key = pred_idx.count)]
 
-    @staticmethod
-    def get_sample(
-        file_path, 
-        sr = 22050, 
-        duration = 3, 
-        n_segments = 5, 
-        n_mfcc = 13, 
-        n_fft = 2048, 
-        hop_len = 512):
-
-        #Check if wav file
-        if file_path.split('\\')[-1].split('.')[-1] != 'wav':
-            print('Only .wav files are allowed for now.')
-            return None
-
-        samp_p_track = sr * duration
-        samp_p_seg = int(samp_p_track / n_segments)
-        mfcc_p_seg = math.ceil(samp_p_seg / hop_len)
-        
-        mfcc_data = []
-        
-        print(f'Loading audio file...')
-        signal, sr = librosa.load(file_path, sr = sr)
-        for part in range((signal.shape[0]//samp_p_track)):
-            sig = signal[samp_p_track*part : (samp_p_track*part) + samp_p_track]
-            for seg in range(n_segments):
-                mfcc = librosa.feature.mfcc(
-                    y = sig[samp_p_seg * seg:(samp_p_seg * seg) + samp_p_seg], 
-                    sr = sr, n_mfcc = n_mfcc, 
-                    n_fft = n_fft, hop_length = hop_len)
-                mfcc = mfcc.T
-                if len(mfcc) == mfcc_p_seg:
-                    mfcc_data.append(mfcc.tolist())
-        return mfcc_data
+    
